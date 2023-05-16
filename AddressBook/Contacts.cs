81e3b424d3f1643dc42dffdf7472ec1f5ff155f7
Tanaya <desaitanaya17@gmail.com>
2023-05-16 16:34:15 +0530

    [Tanaya] Add : C# Program to add a new contact to Address Book.

---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,16 @@
+﻿namespace AddressBook
+{
+
+    public class Contacts
+    {
+        //Getter Setter Methods
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
