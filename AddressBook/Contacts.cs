2fca620b5cfec3736e488102f0a36373495b19a0
Tanaya <desaitanaya17@gmail.com>
2023-05-19 09:35:19 +0530

    [Tanaya] Add : C# Program to remove the contacts in Address Book.

--- AddressBook/Contacts.cs
+++ AddressBook/Contacts.cs
@@ -4,13 +4,7 @@
     public class Contacts
     {
         //Getter Setter Methods
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public int Zip { get; set; }
-        public string State { get; set; }
-        public string Email { get; set; }
-        public int PhoneNumber { get; set; }
+        public string firstName, lastName, address, city, state, email;
+        public int zipCode, phoneNumber;
     }
 }
